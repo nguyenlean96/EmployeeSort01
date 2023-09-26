0198530ebdc2c470c29b68e27c5f04f8e0484484
KienHocCode1 <136813027+KienHocCode1@users.noreply.github.com>
2023-09-25 22:29:47 -0400

    Add files via upload

---
+++ SortingConsole/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingConsole
+{
+    internal class Employee
+    {
+        private string name;
+        private int number;
+        private decimal rate;
+        private double hours;
+        private decimal gross;
+
+        public Employee(string name, int number, decimal rate, double hours)
+        {
+            this.name = name;
+            this.number = number;
+            this.rate = rate;
+            this.hours = hours;
+        }
+        public decimal GetGross() { return gross;}
+        public double GetHours(){ return hours;}
+        public string GetName() { return name;}
+        public int GetNumber() { return number;}
+        public decimal GetRate() { return rate;}
+        public void SetHours(double hours) { this.hours = hours;}
+        public void SetName(string name) { this.name = name;}
+        public void SetNumber(int number) {  this.number = number;}
+        public void SetRate(decimal rate) {  this.rate = rate;}
+        public override string ToString() 
+        {
+            return name + ' ' + number + ' ' + rate + ' ' + hours;
+        }
+
+    }
+}
---
+++ SortingConsole/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingConsole
+{
+    internal class Program
+    {
+        const string DATAFILE = "employees.txt";
+        static void Main(string[] args)
+        {
+            Employee[] employee = new Employee[5];          
+            int numberOfPeople = 0;
+
+            
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.Clear();
+
+                Console.Write("Employee          Number    Rate     Hours    Gross Pay             Kien's Company\n" +
+                    "===============   ======    =====    =====    ==================    --------------\n " +
+                    "1. Sort by Employee Name (ascending)\n " +
+                    "2. Sort by Employee Number (ascending)\n " +
+                    "3. Sort by Employee Pay Rate (descending)\n " +
+                    "4. Sort by Employee Hours (descending)\n " +
+                    "5. Sort by Employee Gross Pay (descending)\n " + 
+                    "6. Exit\n" +
+                    "Enter choice: ");              
+      
+                string number = Console.ReadLine();
+                double option = double.Parse(number);
+                switch (option) 
+                {
+                    case 1:
+                        Console.WriteLine("Hi");
+                        try
+                        {
+                            StreamReader reader = new StreamReader(DATAFILE);
+                            while (!reader.EndOfStream)
+                            {
+                                string[] data = reader.ReadLine().Split(',');
+
+                                string name = data[0];
+                                int id = int.Parse(data[1].Trim());
+                                decimal rate = decimal.Parse(data[2].Trim());
+                                double hourly = double.Parse(data[3].Trim());
+                                employee[numberOfPeople] = new Employee(name, id, rate, hourly);
+                                numberOfPeople++;
+
+                                
+                            }
+                            for (int i = 0; i < numberOfPeople; i++)
+                            {
+                                Console.WriteLine(employee[i]);
+                            }
+                            reader.Close();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Error.WriteLine("Error: " + ex.Message);
+                        }
+
+                        break;
+                    case 2:
+                        for (int i = 0; i < numberOfPeople; i++) 
+                        {
+                            Console.WriteLine(employee[i]);
+                        }
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        
+                        running = false;
+                        break;
+                    default:
+                        Console.Error.WriteLine("Invalid option entered");
+                        break;
+                }
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
